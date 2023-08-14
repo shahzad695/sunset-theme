c14a0192c04b0f86de3e96616a63eed3f4022c5c
Shahzad Ahmad <atifali29786@gmail.com>
2023-08-14 13:12:56 +0500

    Generated sunset-theme custom admin page basic structure

--- public/wp-content/themes/sunset-premium-theme/inc/functions-admin-page.php
+++ public/wp-content/themes/sunset-premium-theme/inc/functions-admin-page.php
@@ -1,14 +1,20 @@
 <?php
         /*  ==============================================
-                        Menu Page functions
+                custom Menu Page generation functions
             ================================================
 
         */
             
 
 function sunset_add_custom_admin_page(){
+    // generate custom menu page
     add_menu_page( 'Sunset Theme Options', 'Sunset', 'manage_options', 'sunset_permium', 'sunset_custom_menu_page',
     get_template_directory_uri().'/img/sunset-icon.png', 110);
+    // generate custom sbmenu pages
+
+    add_submenu_page('sunset_permium', 'general-settings', 'General', 'manage_options', 'sunset_permium', 'sunset_custom_menu_page');
+    add_submenu_page('sunset_permium', 'custom-css', 'custom_css', 'manage_options', 'sunset_permium_css', 'sunset_custom_css_page');
+
 }
 
         /*  ==============================================
@@ -29,4 +35,7 @@ function sunset_add_custom_admin_page(){
 
 function sunset_custom_menu_page(){
     echo '<h1>Sunset Theme Options</h1>';
+}
+function sunset_custom_css_page(){
+    echo '<h1>Custom Css page</h1>';
 }
