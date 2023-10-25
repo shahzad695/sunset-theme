50e05ee06f05f51c4580380cea1f764cdfb75c07
Shahzad Ahmad <atifali29786@gmail.com>
2023-10-26 00:18:32 +0500

    developed the header area and added the other files for frontend of the theme

--- public/wp-content/themes/sunset-premium-theme/header.php
+++ public/wp-content/themes/sunset-premium-theme/header.php
@@ -1,2 +1,24 @@
-<?php
-wp_head();
+<html lang="<?php language_attributes(); ?>">
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('title'); ?></title>
+    <?php wp_head();?>
+</head>
+
+<body <?php body_class()?>>
+    <header class="header">
+        <div class="header__image" style="background-image:url('<?php header_image()?>')">
+
+        </div>
+        <div class="header__content ">
+
+            <h1 class="sunset-icon">
+                <span class="sunset-logo"></span>
+                <span class="hide"><?php bloginfo('name')?></span>
+            </h1>
+            <h2 class="header__descrip"><?php bloginfo('description')?></h2>
+        </div>
+        <nav class="header__nav"></nav>
+    </header>
