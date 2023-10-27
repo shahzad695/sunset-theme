3f4a4c609cc8280a2ed29a554cfea494f0f3629d
Shahzad Ahmad <atifali29786@gmail.com>
2023-10-27 11:10:08 +0500

    added the npm and webpack workflow for styles management

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "sunset-frontend-compiled.css",
+    }),
+  ],
+  mode: "production",
+  watch: true,
+  entry:
+    "./public/wp-content/themes/sunset-premium-theme/sass/sunset-frontend.scss",
+  output: {
+    filename: "sunset-frontend-js-compiled.js",
+    path: path.resolve(
+      __dirname,
+      "public/wp-content/themes/sunset-premium-theme/final-assets"
+    ),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  devtool: "source-map",
+};
