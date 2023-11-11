0a30919296a4591a0408d78468b7759d2a2c73b1
Shahzad Ahmad <atifali29786@gmail.com>
2023-11-11 23:36:38 +0500

    completed the gallery post type

--- public/wp-content/themes/sunset-premium-theme/js/sunset-frontend.js
+++ public/wp-content/themes/sunset-premium-theme/js/sunset-frontend.js
@@ -3,13 +3,20 @@ let carusoleLeftThumb = document.querySelector(".carusole__btn-thumb--left");
 let carusoleNextButton = document.querySelector(".carusole__btn--right");
 let carusolePrevButton = document.querySelector(".carusole__btn--left");
 let carusoleTrack = document.querySelector(".carusole__track");
-let slides = Array.from(carusoleTrack.children);
 let carusoleNav = document.querySelector(".carusole__nav");
+let carusoleItems = document.querySelectorAll(".carusole__item");
+let slides = Array.from(carusoleItems);
 let dots = Array.from(carusoleNav.children);
+console.log(slides);
+console.log(dots);
 
 function moveSlide(currentSlide, targetSlide) {
   currentSlide.classList.remove("carusole__item--current");
   targetSlide.classList.add("carusole__item--current");
+  let nextThumb = targetSlide.dataset.nextimage;
+  carusoleRightThumb.style.backgroundImage = `url(${nextThumb})`;
+  let prevThumb = targetSlide.dataset.previmage;
+  carusoleLeftThumb.style.backgroundImage = `url(${prevThumb})`;
 }
 function moveDot(currentDot, targetDot) {
   currentDot.classList.remove("carusole__dot--current");
@@ -18,23 +25,19 @@ function moveDot(currentDot, targetDot) {
 function elementDeclaration() {
   let currentSlide = carusoleTrack.querySelector(".carusole__item--current");
   let currentDot = carusoleNav.querySelector(".carusole__dot--current");
-  let nextSlide, prevSlide;
+  let nextSlide, prevSlide, nextSlideIndex, prevSlideIndex;
   let currentSlideIndex = slides.findIndex((slide) => slide === currentSlide);
-  let nextSlideIndex = currentSlideIndex + 1;
-  let prevSlideIndex = currentSlideIndex - 1;
+  nextSlideIndex = currentSlideIndex + 1;
+  prevSlideIndex = currentSlideIndex - 1;
 
   if (currentSlideIndex == slides.length - 1) {
     nextSlideIndex = 0;
-    nextSlide = slides[nextSlideIndex];
-    prevSlide = currentSlide.previousElementSibling;
   } else if (currentSlideIndex == 0) {
     prevSlideIndex = slides.length - 1;
-    prevSlide = slides[prevSlideIndex];
-    nextSlide = currentSlide.nextElementSibling;
-  } else {
-    prevSlide = currentSlide.previousElementSibling;
-    nextSlide = currentSlide.nextElementSibling;
   }
+
+  prevSlide = slides[prevSlideIndex];
+  nextSlide = slides[nextSlideIndex];
   return {
     currentSlide,
     nextSlide,
@@ -46,7 +49,6 @@ function elementDeclaration() {
   };
 }
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Loading");
   let {
     currentSlide,
     nextSlide,
@@ -60,12 +62,6 @@ document.addEventListener("DOMContentLoaded", () => {
   carusoleLeftThumb.style.backgroundImage = `url(${prevThumb})`;
   let nextThumb = currentSlide.dataset.nextimage;
   carusoleRightThumb.style.backgroundImage = `url(${nextThumb})`;
-  console.log(
-    prevSlide,
-    nextSlide,
-    carusoleRightThumb.style.backgroundImage,
-    carusoleLeftThumb.style.backgroundImage
-  );
 });
 carusoleNextButton.addEventListener("click", function (e) {
   let {
@@ -79,10 +75,7 @@ carusoleNextButton.addEventListener("click", function (e) {
   } = elementDeclaration();
 
   let nextDot = dots[nextSlideIndex];
-  let nextThumb = nextSlide.dataset.nextimage;
-  carusoleRightThumb.style.backgroundImage = `url(${nextThumb})`;
-  let prevThumb = nextSlide.dataset.previmage;
-  carusoleLeftThumb.style.backgroundImage = `url(${prevThumb})`;
+
   moveSlide(currentSlide, nextSlide);
   moveDot(currentDot, nextDot);
 });
@@ -98,16 +91,13 @@ carusolePrevButton.addEventListener("click", function (e) {
   } = elementDeclaration();
 
   let prevDot = dots[prevSlideIndex];
-  let prevThumb = prevSlide.dataset.previmage;
-  carusoleLeftThumb.style.backgroundImage = `url(${prevThumb})`;
-  let nextThumb = prevSlide.dataset.nextimage;
-  carusoleRightThumb.style.backgroundImage = `url(${nextThumb})`;
   moveSlide(currentSlide, prevSlide);
   moveDot(currentDot, prevDot);
 });
 
 carusoleNav.addEventListener("click", function (e) {
   targetDot = e.target.closest("button");
+  console.log(targetDot);
   if (!targetDot) return;
   let currentSlide = document.querySelector(".carusole__item--current");
   let currentDot = document.querySelector(".carusole__dot--current");
--- public/wp-content/themes/sunset-premium-theme/template-parts/content-gallery.php
+++ public/wp-content/themes/sunset-premium-theme/template-parts/content-gallery.php
@@ -2,7 +2,6 @@
     <header class="post__header">
         <?php if(sunset_retrive_pics()){
             $thumbnailurl= sunset_retrive_pics(7);
-            
             }?>
         <div class="carusole">
         <div class="carusole__btn-container">
@@ -12,18 +11,23 @@
             <div class="carusole__container">
                 <ul class="carusole__track">
                     <?php 
+                        
                         $count = count($thumbnailurl)-1;
                     for ($i=0; $i <= $count; $i++){
                         $prevNum =($i==0) ? $count : $i-1;
                         $prevThumbUrl = $thumbnailurl[$prevNum];
                         $nextNum = ($i == $count) ? 0 : $i+1;
                         $nextThumbUrl = $thumbnailurl[$nextNum];
+                        
                         $currentSlide = '';
                         if ($i == 0){$currentSlide = 'carusole__item--current';}
                         ?>
                     <li class="carusole__item <?php echo $currentSlide?>" data-previmage="<?php echo wp_get_attachment_thumb_url($prevThumbUrl->ID);?>"
                     data-nextimage="<?php echo wp_get_attachment_thumb_url($nextThumbUrl->ID);?>"  style="background-image:url('<?php echo wp_get_attachment_url($thumbnailurl[$i]->ID); ?>') ">
                     </li>
+                        <summary class="post__excerpt post__excerpt--gallery <?php echo $currentSlide?>">
+                            <?php echo $thumbnailurl[$i]->post_excerpt;?>
+                        </summary>
                     <?php } ?>
                 </ul>
                 <div class="carusole__nav">
@@ -31,6 +35,7 @@
                         $currentDot = '';
                         if ($i == 1){$currentDot = 'carusole__dot--current';}
                         ?>
+                        
                     <button class="carusole__dot <?php echo $currentDot?> "></button>
                     <?php } ?>
 
@@ -49,9 +54,7 @@
     </header>
     <div class="post__content">
 
-        <summary class="post__excerpt">
-            <?php the_excerpt();?>
-        </summary>
+    
 
         <a href="<?php the_permalink();?>" class="link link--btn btn ">Read More</a>
 
