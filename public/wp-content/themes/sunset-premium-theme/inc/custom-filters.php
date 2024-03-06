7c9b212168883d8a794d31754dd8a774f258ef0f
Shahzad Ahmad <atifali29786@gmail.com>
2024-03-06 21:45:17 +0500

    added the social media section to single php files through the use of filter

--- public/wp-content/themes/sunset-premium-theme/inc/custom-filters.php
+++ public/wp-content/themes/sunset-premium-theme/inc/custom-filters.php
@@ -28,3 +28,31 @@ function posts_link_prev_class($format) {
   return $format;
 }
 add_filter('previous_post_link', 'posts_link_prev_class');
+
+ /*     =====================================
+            custom social media links
+        =====================================*/
+
+function sunset_social_media_links($content){
+
+  if(is_single()){
+    $content.='<div class="post__social"><h1 class="post__social--heading">Share This</h1></div>';
+
+  $title =get_the_title();
+  $permalink= get_the_permalink();
+  $twitterHandler= (get_option('twitterhandler') ? '&amp;via='.esc_attr( get_option('twitterhandler')).'' : '');
+
+  $twitter ='https://twitter.com/intent/tweet?text=hey read this'.$title.'&amp;url='.$permalink. $twitterHandler.'';
+  $facebook ='https://www.facebook.com/sharer/sharer.php?u='.$permalink.'';
+  $google ='https://plus.google.com/share?url='.$permalink.'';
+  $content.='<ul class="Post__social--links">';
+  $content.='<li><a href='.$twitter.' target="_blank" class="link"><span class="sunset-icon sunset-twitter"></span></a></li>'; 
+  $content.='<li><a href='.$facebook.' target="_blank" class="link"><span class="sunset-icon sunset-facebook"></span></a></li>'; 
+  $content.='<li><a href='.$google.' target="_blank" class="link"><span class="sunset-icon sunset-googleplus"></span></a></li>'; 
+  $content.='</ul>';
+
+  return $content;}else{
+    return $content;
+  }
+}
+add_filter('the_content', 'sunset_social_media_links');
